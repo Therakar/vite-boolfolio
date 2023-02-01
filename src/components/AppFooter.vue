42f7818e997cc74eb5f891824d93c4d8c710eaf5
Therakar <matteo.pau200@gmail.com>
2023-02-01 20:09:06 +0100

    installazione axios e  scaffolding

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "AppFooter",
+};
+</script>
+
+<template>
+  <footer>
+    <h1>footer content</h1>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header>
+    <h1>Logo</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
