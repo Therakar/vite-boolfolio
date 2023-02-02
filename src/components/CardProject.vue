4dff7cb2c7d45fd2659118723cba1dbda9ee31fe
Therakar <matteo.pau200@gmail.com>
2023-02-02 18:08:19 +0100

    stampa correttamente tipo e tecnologie del progetto nella card e in caso un dato non sia presente stamapa un "No Type/Technology"

--- src/components/CardProject.vue
+++ src/components/CardProject.vue
@@ -14,16 +14,31 @@
 
 <template>
     <div class="card" style="width: 18rem;">
-  <img v-if="data.image_url" :src="data.image_url" class="card-img-top" :alt="data_title">
-  <div class="card-body">
-    <h5 class="card-title">{{data.title}}</h5>
-    <p class="card-text">Description: {{ truncateText(data.description) }}</p>
-    <p class="card-text">Version: v{{ data.version }}</p>
-    <p class="card-text">Customer: {{ data.customer }}</p>
-    <p class="card-text">Slug: {{ data.slug}}</p>
-    <p class="card-text">Type: {{ data.type.name }}</p>
-    <p class="card-text">Technologies: {{ data.technologies.name }} </p>
-  </div>
+        <img v-if="data.image_url" :src="data.image_url" class="card-img-top" :alt="data.title">
+    <div class="card-body">
+        <h5 class="card-title">{{data.title}}</h5>
+        <p class="card-text">Description: {{ truncateText(data.description) }}</p>
+        <p class="card-text">Version: v{{ data.version }}</p>
+        <p class="card-text">Customer: {{ data.customer }}</p>
+        <p class="card-text">Slug: {{ data.slug}}</p>
+        <div class="card-text">
+            <p v-if="data.type">Type: {{data.type.name}} </p>
+            <p v-else>Type: No Type</p>
+        </div>
+        <div class="card-text">
+            Technology:
+            <div v-if="data.technologies.length > 0">
+                <span class="badge text-bg-dark me-1" v-for="technology in data.technologies">{{technology.name}}</span>
+            </div>
+            <div v-else>
+                <span class="badge text-bg-dark" >No Technology</span>
+            </div>
+        </div>
+        
+
+        
+        
+    </div>
 </div>
 
 </template>
