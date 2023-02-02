bbf995e9c4bed7de3cc2d2026acdb4cd60df26c7
Therakar <matteo.pau200@gmail.com>
2023-02-02 18:43:54 +0100

    rrende le card che si trovano in una stessa row della stessa altezza

--- src/components/CardProject.vue
+++ src/components/CardProject.vue
@@ -13,7 +13,7 @@
 </script>
 
 <template>
-    <div class="card" style="width: 18rem;">
+    <div class="card h-100" style="width: 18rem;">
         <img v-if="data.image_url" :src="data.image_url" class="card-img-top" :alt="data.title">
         <div class="card-body">
             <h5 class="card-title">{{data.title}}</h5>
